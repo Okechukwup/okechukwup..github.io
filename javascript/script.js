74b85948934734d6bdce1465b379857f17922822
Okechukwu Prince <princeowilson@gmail.com>
2021-11-06 15:45:52 +0100

    More Updates

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,4 @@ let currentYear = date.getFullYear();
 document.querySelector('#current-year').textContent = currentYear;
 
 let lastModified = document.lastModified;
-document.getElementById('last-modified').textContent = lastModified;
+document.getElementById('#last-modified').textContent = lastModified;
